755a52a8379e1d04bac2f531d4b78b35fb71593d
Vladislav Murashko <vladislav.murashko@axonim.by>
2020-11-21 19:07:44 +0300

    Add DrawView, add clear button

---
+++ Paint/app/src/main/java/by/bsuir/murashko/paint/DrawView.kt
@@ -0,0 +1,113 @@
+package by.bsuir.murashko.paint
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import kotlin.math.abs
+
+private const val STROKE_WIDTH = 12f
+
+class DrawView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+
+    private var path = Path()
+    private val drawColor = Color.BLACK
+    private val backgroundColor = Color.WHITE
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    /**
+     * Don't draw every single pixel.
+     * If the finger has has moved less than this distance, don't draw. scaledTouchSlop, returns
+     * the distance in pixels a touch can wander before we think the user is scrolling.
+     */
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    fun clearCanvas() {
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+    /**
+     * The following methods factor out what happens for different touch events,
+     * as determined by the onTouchEvent() when statement.
+     * This keeps the when conditional block
+     * concise and makes it easier to change what happens for each event.
+     * No need to call invalidate because we are not drawing anything.
+     */
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(
+                currentX,
+                currentY,
+                (motionTouchEventX + currentX) / 2,
+                (motionTouchEventY + currentY) / 2
+            )
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to save it.
+            extraCanvas.drawPath(path, paint)
+        }
+
+        invalidate()
+    }
+
+    private fun touchUp() {
+        path.reset()
+    }
+}
--- Paint/app/src/main/java/by/bsuir/murashko/paint/MainActivity.kt
+++ Paint/app/src/main/java/by/bsuir/murashko/paint/MainActivity.kt
@@ -1,11 +1,20 @@
 package by.bsuir.murashko.paint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawView: DrawView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawView = findViewById(R.id.draw_view)
+    }
+
+    fun clearCanvas(view: View) {
+        drawView.clearCanvas()
     }
 }
